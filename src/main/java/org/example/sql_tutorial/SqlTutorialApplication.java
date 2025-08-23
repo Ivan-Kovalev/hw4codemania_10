64bcffce855934c476e36e82be4233016fd776ab
Ivan-Kovalev <gangrenresdwa@gmail.com>
2025-08-23 10:56:58 +0300

    master

---
+++ src/main/java/org/example/sql_tutorial/SqlTutorialApplication.java
@@ -0,0 +1,13 @@
+package org.example.sql_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlTutorialApplication.class, args);
+    }
+
+}
